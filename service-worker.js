136aad2d86769565e261755d4b1a9d24a568580f
yanufa <yanufaindo@gmail.com>
2026-09-19 15:55:16 +0700

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'stokjas-v1';
+var urlsToCache = [
+  './index2.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.indexOf('script.google.com') !== -1 ||
+      event.request.url.indexOf('googleusercontent.com') !== -1) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
